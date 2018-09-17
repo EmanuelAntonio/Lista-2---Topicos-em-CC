c34c988223102eedb48d26ab5f025f3669881d9c
Lucas Frank <lucas.r.frank@gmail.com>
2018-09-16 21:08:09 -0300

    Tentando arrumar o LxF e tentativa de KT mas com problemas na implementação

--- lista2.py
+++ lista2.py
@@ -6,8 +6,7 @@ import math as m
 def exata(x):
     y = np.zeros(len(x))
     for i in range(len(x)):
-        if(x[i] <= 0.55):
-            y[i] = 1.0
+        y[i] = (x[i] ** 2) / 2
     return y
 
 
@@ -21,7 +20,7 @@ def laxFriedrichs(uu,Nx,Nt,h,dt):
     u = np.zeros([Nx+1,Nt+1])
     u[:,:] = uu[:,:]
     for t in range(0,Nt):
-        for i in range(1,Nx):
+        for i in range(1,Nx-1):
             u[i,t+1] = 0.5*(u[i+1,t]+u[i-1,t]) - (K*dt/(2*h))*(u[i+1,t]-u[i-1,t])
     return u
 
@@ -30,8 +29,47 @@ def laxFriedrichs(uu,Nx,Nt,h,dt):
     Método Kurganov-Tadmor - KT
 */
 """
-def kurganovTadmor():
-    return
+def kurganovTadmor(uu,Nx,Nt,h,dt):
+    u = np.zeros([Nx+1,Nt+1])
+    u[:,:] = uu[:,:]
+
+    for t in range(0,Nt):
+        for j in range(1,Nx-1):
+            u[j,t] = - (H(u,j+0.5,t,h)-H(u,j-0.5,t,h))/h
+
+    return u
+
+def H(u,j,t,h):
+    return 0.5*(f(uPlus(u,j,t,h)) + f(uMinus(u,j,t,h))) - (0.5*a(u,j,t,h))*(uPlus(u,j,t,h) - uMinus(u,j,t,h))
+
+def uPlus(u,j,t,h):
+    up[j,t] = u[j + 0.5,t] - 0.5*h*uX(u,j + 0.5,t,h)
+    return up
+
+def uMinus(u,j,t,h):
+    um[j,t] = u[j - 0.5] + 0.5*h*uX(u,j-0.5,t,h)
+    return um
+
+def uX(u,j,t,h):
+    lista = list()
+    lista.append(2*(u[j,t] - u[j-1,t])/h)
+    lista.append((u[j+1,t] - u[j-1,t])/2*h)
+    lista.append(2*(u[j+1,t] - u[j,t])/h)
+
+    resultado = min(lista)
+
+    return abs(resultado)
+
+def a(u,j,t,h):
+    return max(derivadaF(uPlus(u,j,t,h)),derivadaF(uMinus(u,j,t,h)))
+
+def f(x):
+    return x
+
+def derivadaF(x):
+    return x
+
+############################################################
 
 def condicaoInicial(Nx,Nt,h):
     u = np.zeros([Nx+1,Nt+1])
@@ -42,16 +80,18 @@ def condicaoInicial(Nx,Nt,h):
 
 def condicoesContorno(u,Nx,Nt):
     for t in range(0, Nt+1):
-        u[0,t] = 0.0
-        u[Nx,t] = m.sin(Nx*h)
+        u[0,t] = m.sin(0*h)
+        u[1,t] = m.sin(1*h)
+        u[Nx-2,t] = m.sin((Nx-2)*h)
+        u[Nx-1,t] = m.sin((Nx-1)*h)
     return u
 
 #Entrada
 A = 0 #Limite inferior para x
 B = m.pi #Limite superior para x
-Nx = 100 #Quantidade de elementos
+Nx = 102 #Quantidade de elementos + (inicio + final) -> 2
 h = (B - A)/np.float(Nx) #Discretização no espaço
-T = 1 #Tempo final
+T = 4 #Tempo final
 dt = 0.5*h #Discretização no tempo
 Nt = np.int(T/dt) #Quantidade de iterações no tempo
 x = np.linspace(A,B,Nx+1) #Para plot das aproximações
@@ -64,6 +104,7 @@ u = condicaoInicial(Nx,Nt,h)
 u = condicoesContorno(u,Nx,Nt)
 
 u_laxFri = laxFriedrichs(u,Nx,Nt,h,dt)
+#u_kt = kurganovTadmor(u,Nx,Nt,h,dt)
 
 tj = 0 #Instante de tempo desejado
 t = int(tj/dt) #Índice correspondente
